37dc1e5e1e7d952da2c73fa296144a9c330ceb87
John D <john@MacBook-Pro-de-John-2.local>
2016-12-19 23:17:54 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>Perfection is achieved, not when there is nothing more to add, but when there is nothing left to take away.</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
