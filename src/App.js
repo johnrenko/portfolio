9a15a08b868baa4c42dabccdfbebfad1d3818cca
John Dutamby <vishnou@gmail.com>
2020-06-27 16:07:31 +0200

    lettrage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import lettrage from "./lettrage.png";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h1>Perfection is achieved, not when there is nothing more to add, but when there is nothing left to take away.</h1>
+          <img src={lettrage} alt="John" />
         </div>
       </div>
     );
